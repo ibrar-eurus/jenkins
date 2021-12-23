52670a914dff4939680251db44be0d5348dfc976
ibrar-eurus <ibrar.ullah@eurustechnologies.com>
2021-12-23 14:38:41 +0500

    remove some ting from line18

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('ibrareurus3344/jobdsl-demo:tagname')
+            repositoryName('ibrareurus3344/jobdsl-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
