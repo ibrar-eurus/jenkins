0a852af18c6a72189b808cacb34d7725ed436d1d
ibrar-eurus <ibrar.ullah@eurustechnologies.com>
2021-12-23 12:49:58 +0500

    add own repo name

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('ibrareurus3344/jobdsl-demo:tagname')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
